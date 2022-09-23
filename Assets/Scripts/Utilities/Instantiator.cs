fba12c0391c9dfbf213522955c631342419db6c4
Cabrera Juan Manuel <63826558+elpibealfajor@users.noreply.github.com>
2022-09-23 20:05:27 -0300

    SpawnPoints

--- Assets/Scripts/Utilities/Instantiator.cs
+++ Assets/Scripts/Utilities/Instantiator.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.Linq;
 
 public class Instantiator : MonoBehaviour
 {
-    [SerializeField] private Transform spawn;
+    [SerializeField] private List<Transform> spawns;
     void Start()
     {
-        PhotonNetwork.Instantiate("Player", spawn.position, Quaternion.identity);
+        List<Photon.Realtime.Player> playerList = PhotonNetwork.PlayerList.ToList();
+        switch (playerList.Count)
+        {
+            case 1:
+                PhotonNetwork.Instantiate("Player", spawns[0].position, Quaternion.identity);
+                break;
+            case 2:
+                PhotonNetwork.Instantiate("Player2", spawns[1].position, Quaternion.identity);
+                break;
+            case 3:
+                PhotonNetwork.Instantiate("Player3", spawns[2].position, Quaternion.identity);
+                break;
+            case 4:
+                PhotonNetwork.Instantiate("Player4", spawns[3].position, Quaternion.identity);
+                break;
+        }
     }
 }
